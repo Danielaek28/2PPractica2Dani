c1bfc06208d144e7914be7ec84ff65ccacc1d78c
danielaek <ekmex.daniela@gmail.com>
2023-08-01 22:02:25 -0600

    Primer commit

---
+++ app/src/main/java/com/example/sp2practica2_dani/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sp2practica2_dani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/sp2practica2_dani/expositor.java
@@ -0,0 +1,14 @@
+package com.example.sp2practica2_dani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class expositor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expositor);
+    }
+}
